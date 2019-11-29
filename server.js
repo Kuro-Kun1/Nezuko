4fd767480cb8c25903674cd895ef35bb0b61ee52
Kuro-Kun1 <57526046+Kuro-Kun1@users.noreply.github.com>
2019-11-29 19:01:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
+const config = require('./config.json');
 
 client.on('ready', () => {
-  console.log(`Online In Discord! ${client.user.tag}!`);
+
+client.user.setActivity('Glitch', { type: 'PLAYING' })
+  console.log(`Online in Discord!! ${client.user.tag}!`);
+});
+
+client.on('message', async message => {  
+if (message.author.bot) return;
+if (message.channel.type === 'dm') return;
+ 
+  let prefix = config.prefix
+  let messageArray = message.content.split(' ');
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  
+   if (cmd === `${prefix}help`)  {
+     let bicon = client.user.displayAvatarURL;
+     let embed = new Discord.RichEmbed()
+     .setAuthor("―――――Help Command―――――", client.user.avatarURL)
+     .setColor("RANDOM")
+     .setThumbnail(bicon)
+     .addField("**Music Command :**", 
+     `**|** _**!PLAY**_  **|**  _**!SKIP**_  **|**  _**!STOP**_  **|**  _**!PAUSE**_  **|**`)
+     .addField("**Dibuat Oleh Kuro :**", client.user.createdAt)
+      message.channel.send(embed)
+   }
 });
 
 client.on('message', msg => {
@@ -11,4 +36,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('process.env.TOKEN');
+client.login(process.env.TOKEN);
